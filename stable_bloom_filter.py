1f4fb276ba0013986eaf0628855f3b669a531744
Shrikant Garg <shrikant18312@iiitd.ac.in>
2022-04-10 00:38:19 +0530

    implemented is_duplicate

--- stable_bloom_filter.py
+++ stable_bloom_filter.py
@@ -1,20 +1,20 @@
 class stable_bloom_filter(object):
     
-    def __init__(N,Mx,K):
+    def __init__(m,Mx,K):
         '''
-        N: Number of Cells in the bloom filter
+        m: Number of Cells in the bloom filter
         Mx: Maximum value a cell can hold
         K: Number of Hash Functions
         
         Initialises a stable bloom filter
-        '''
-        
-        self.N=N
+        ''' 
+        self.m=m
         self.Mx=Mx
         self.K=K
         
         #calculating other parameters.
         self.P=5
+        self.filter=[0]*self.m
         
     def is_duplicate(item):
         '''
@@ -22,7 +22,15 @@ class stable_bloom_filter(object):
         
         returns if the input item is already contained within the filter
         '''
-        pass
+        flag=True
+        hashes=get_hashes()
+        for i in range(self.K):
+            if(hashes[i]==0):
+                flag=False
+
+        remove_random_elements()
+        add_item(item)
+        return flag
     
     def add_element(item):
         '''
